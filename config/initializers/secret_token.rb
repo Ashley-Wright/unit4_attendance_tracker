a7ca69351a3b2c318e216cd8caeea3486860e58e
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-02-20 01:30:16 -0600

    Initialized Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unit4AttendanceTracker::Application.config.secret_key_base = '9cd600776f94db1ca633e3435087692cecfc2c4e8f25489cd8fded04909018dc36687987acd544075071cfed4203480387f39a2ef97a21f1999f22fda67bfc69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unit4AttendanceTracker::Application.config.session_store :cookie_store, key: '_unit4_attendance_tracker_session'
