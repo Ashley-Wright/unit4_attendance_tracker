737fd30aaca3c5321cbd09c1d61804f3678a8a03
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-03-10 12:26:29 -0500

    Generate random code saves entry in Meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,7 @@
 class MeetingsController < ApplicationController
   def create
     @code = 6.times.map{[*'0'..'9',*'A'..'Z'].sample}.join
+    course = Course.find_by_id(params[:course_id])
+    Meeting.create!(course: course, code: @code)
   end
 end
